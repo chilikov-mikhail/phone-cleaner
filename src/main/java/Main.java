b1388c1442056d0a002b85ba0ccc95da19cb1431
ChMV <chilikov_mihail@mail.ru>
2021-12-21 02:16:50 +0400

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+      String input = scanner.nextLine();
+      if (input.equals("0")) {
+        break;
+      }
+      //TODO:напишите ваш код тут, результат вывести в консоль.
+      input = input.replaceAll("[^0-9]", "");
+      if (Pattern.matches("^7[0-9]{10}$", input)) {
+        System.out.println(input);
+      } else if (Pattern.matches("^8[0-9]{10}$", input)){
+        System.out.println("7" + input.substring(1));
+      } else if (Pattern.matches("^[0-9]{10}$", input)) {
+        System.out.println("7" + input);
+      } else {
+        System.out.println("Неверный формат номера");
+      }
+    }
+  }
+
+}
